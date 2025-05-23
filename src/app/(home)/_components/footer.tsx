b7e13ea9bd8bfa8c54143b789ded252bd956ec18
fzl96 <fpsdragon333@gmail.com>
2025-05-24 03:34:08 +0700

    feat: Customize home layout

---
+++ src/app/(home)/_components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="flex border-t justify-between font-medium p-6">
+      <div>
+        <p>Wisethetic</p>
+      </div>
+    </footer>
+  );
+}
